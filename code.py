10c28ff402bbb7dd11c56230d08f7acc558ec8be
nadiand <78037564+nadiand@users.noreply.github.com>
2022-12-14 14:34:34 +0100

    Improve comments

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-# import relevant libraries
+# Import relevant libraries
 from beir.datasets.data_loader import GenericDataLoader
 from beir import util
 import pathlib, os
@@ -17,7 +17,7 @@ from beir.retrieval.train import TrainRetriever
 import torch
 
 def DCG(query_relevancy_labels, k):
-  ### calculate the Discounted Cumulative Gain @k
+  ### Calculate the Discounted Cumulative Gain @k
   result = 0
 
   for i in range(0, min(k, len(query_relevancy_labels))):
@@ -28,7 +28,7 @@ def DCG(query_relevancy_labels, k):
 
 
 def NDCG(query_relevancy_labels, qrels, k):
-  ### calculate the Normalized Discounted Cumulative Gain @k
+  ### Calculate the Normalized Discounted Cumulative Gain @k
   numerator = DCG(query_relevancy_labels, k)
   denominator = DCG(qrels, k)
 
@@ -38,31 +38,31 @@ def NDCG(query_relevancy_labels, qrels, k):
     return 0
 
 def NDCG_allqueries(query_relevancy_labels, qrels):
-  ### calculate the Normalized Discounted Cumulative Gain @k for each ranking and qrels in a dictionary of multiple.
+  ### Calculate the Normalized Discounted Cumulative Gain @k for each ranking and qrels in a dictionary of multiple.
   score = 0
 
-  # for each query
+  # For each query
   for key in query_relevancy_labels.keys():
 
-    # sort the dictionary on relevancy
+    # Sort the dictionary on relevancy
     dictionary = query_relevancy_labels[key]
     items = list(dictionary.items())
     dictionary = dict(sorted(items, reverse=True, key= lambda x: x[1]))
 
-    # get the labels and qrels such that NDCG@k can be calculated 
+    # Get the labels and qrels such that NDCG@k can be calculated 
     dictionary_qrels = qrels[key]
     qrels_labels = list(dictionary_qrels.values())
     qrels_labels.sort(reverse=True)
     ranking = list(dictionary.keys()) 
     labels = list(map(lambda x: dictionary_qrels[x] if x in dictionary_qrels.keys() else 0, ranking))
 
-    # calculate the NDCG@10
+    # Calculate the NDCG@10
     result = NDCG(labels, qrels_labels, 10)
     score += result
   return score/len(query_relevancy_labels)
     
 def load_dataset_traintest(dataset):
-  ### load the train and test set for a given dataset
+  ### Load the train and test set for a given dataset
   
   url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
   out_dir = os.path.join(pathlib.Path(os.path.dirname(sys.executable)).parent.absolute(), "datasets")
@@ -77,7 +77,7 @@ def load_dataset_traintest(dataset):
   return train_corpus, train_queries, train_qrels, test_corpus, test_queries, test_qrels
 
 def load_dataset(dataset):
-  ### load just the test set for a given dataset
+  ### Load just the test set for a given dataset
   
   url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
   out_dir = os.path.join(pathlib.Path(os.path.dirname(sys.executable)).parent.absolute(), "datasets")
@@ -88,7 +88,7 @@ def load_dataset(dataset):
 
 
 def finetune(corpus, queries, qrels, pyserini_qrels, nr_queries=1000, num_epochs = 3, evaluation_steps = 1000):
-  ####### finetune BERT model on corpus, queries, qrels and pyserini_qrels. Possibly given some number of queries, epochs and evaluation steps #######
+  ####### Finetune BERT model on corpus, queries, qrels and pyserini_qrels. Possibly given some number of queries, epochs and evaluation steps #######
   
   ### Start with initial BERT model and set a batch-size on the train retriever
   model_finetuned = SentenceTransformer("msmarco-distilbert-base-v3")
@@ -120,7 +120,7 @@ def finetune(corpus, queries, qrels, pyserini_qrels, nr_queries=1000, num_epochs
   save_path = './tmp_model/'
   retriever_finetuned.model.save(save_path, model_name='tmp')
 
-  # return model path
+  # Return model path
   return save_path
 
 def get_pyeserini(index, queries):
@@ -152,7 +152,7 @@ def finetuned_bert(model, corpus, queries, pyserini):
   return reranked_results
 
 def get_NDCGs(pyserini, standard, finetuned, qrels):
-  ### print(qrels)
+  ### Display the evaluation results
   relevancy_judgements_pyserini = pyserini
   relevancy_judgements_standard = standard
   relevancy_judgements_finetuned = finetuned
@@ -163,40 +163,40 @@ def get_NDCGs(pyserini, standard, finetuned, qrels):
   print()
 
 def run_tests(finetune_dataset, index, test_datasets_indexes):
-  ### run BM25, BERT and BERT-F on the given datasets
+  ### Run BM25, BERT and BERT-F on the given datasets
   
-  # load the train data and finetune our model on it
+  # Load the train data and finetune our model on it
   train_corpus, train_queries, train_qrels, test_corpus, test_queries, test_qrels = load_dataset_traintest(finetune_dataset)
   pyserini_qrels = get_pyeserini(index, train_queries)
-  model = finetune(train_corpus, train_queries, train_qrels, pyserini_qrels, nr_queries=1000, num_epochs = 5, evaluation_steps = 100) #TODO rmeove
+  model = finetune(train_corpus, train_queries, train_qrels, pyserini_qrels, nr_queries=1000, num_epochs = 5, evaluation_steps = 100)
 
   def run_test(model, corpus, queries, index, qrels):
-    ### function to run one test
+    ### Run a single test
     
-    # rerank using pyserini
+    # Rerank using pyserini
     pyserini_results_nfcorpus = get_pyeserini(index, queries)
     
-    # rerank using BERT
+    # Rerank using BERT
     reranked_results = standard_bert(corpus, queries, pyserini_results_nfcorpus)
     
-    # rerank using BERT-F
+    # Rerank using BERT-F
     reranked_results_finetuned = finetuned_bert(model, corpus, queries, pyserini_results_nfcorpus)
     
-    # calculate and display average NDCG@10 for all 3 rankings
+    # Calculate and display average NDCG@10 for all 3 rankings
     get_NDCGs(pyserini_results_nfcorpus, reranked_results, reranked_results_finetuned, qrels)
     
-    # free used memory because Python did not do this automatically :D
+    # Free used memory because Python did not do this automatically :D
     gc.collect()
 
-  # run validation on train-set to measure whether our fine-tuning works as expected
+  # Run validation on train-set to measure whether our fine-tuning works as expected
   print(f"Results for {finetune_dataset}-train with Bert finetuned on {finetune_dataset}")
   run_test(model, train_corpus, train_queries,index, train_qrels)
 
-  # run validation on test-set related to our training data
+  # Run validation on test-set related to our training data
   print(f"Results for {finetune_dataset} with Bert finetuned on {finetune_dataset}")
   run_test(model, test_corpus, test_queries, index, test_qrels)
 
-  # run test with our finetuned model on all other given test sets
+  # Run test with our finetuned model on all other given test sets
   for test_dataset, test_index in test_datasets_indexes:
     try:
       print(f"Results for {test_dataset} with Bert finetuned on {finetune_dataset}")
@@ -205,7 +205,7 @@ def run_tests(finetune_dataset, index, test_datasets_indexes):
     except:
       print("failed")
       
-# define finetune and test dataset for our experiments
+# Define finetune and test dataset for our experiments
 finetune_dataset, index = "nfcorpus", "beir-v1.0.0-nfcorpus-flat"
 test_datasets = [
     ("quora", "beir-v1.0.0-quora-flat"),
@@ -215,12 +215,13 @@ test_datasets = [
     ("fiqa", "beir-v1.0.0-fiqa-flat")
 ]
 
-# run tests on our given datasets
+# Run tests on our given datasets
 run_tests(finetune_dataset, index, test_datasets)
 
 #### EXTRA EXPERIMENT ####
-# To confirm how good/bad our ranking performs, we compared it's performance to a random reranking of the top 100 documents as retreived by BM25
-# The following code allows for random reranking and evaluation of the results. This is not part of the actual experiments, but was used as a baseline
+# To confirm how good/bad our ranking performs, we compared its performance to a random reranking of the top 100 documents as retreived by BM25
+# The following code allows for random reranking and evaluation of the results. This is not part of the actual experiments, but was used as a 
+# baseline and sanity check
 import random
 
 def shuffle(lst):
